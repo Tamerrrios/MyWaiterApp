c44ae9494d7d63112c9e4aaa4cb84f9e8ee4555e
Temur <99859548+Tamerrrios@users.noreply.github.com>
2022-07-28 17:45:31 +0500

    Add files via upload

---
+++ MyWaiter/MyWaiter/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MyWaiter
+//
+//  Created by Temur Juraev on 28.07.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, worlddd!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MyWaiter/MyWaiter/MyWaiterApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyWaiterApp.swift
+//  MyWaiter
+//
+//  Created by Temur Juraev on 28.07.2022.
+//
+
+import SwiftUI
+
+@main
+struct MyWaiterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
